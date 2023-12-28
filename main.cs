792db5c2ce2ad8c4e40880e22f56045db6edd994
SonoWindows <133232237+SonoWindows@users.noreply.github.com>
2023-12-28 20:28:09 +0100

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Firewall_Disabler
+{
+    public class main
+    {
+        public static void Main()
+        {
+            ProcessStartInfo disable_firewall = new ProcessStartInfo();
+            disable_firewall.FileName = @"netsh.exe";
+            disable_firewall.Arguments = @"advfirewall set allprofiles state off";
+            disable_firewall.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(disable_firewall);
+
+
+            Environment.Exit(0);
+        }
+    }
+}
